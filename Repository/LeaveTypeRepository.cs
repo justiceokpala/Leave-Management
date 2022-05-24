0c024d1f94209bcc24c5ec22703ac7a84571dfee
justice <justice@DESKTOP-2ROALRR>
2022-05-24 10:36:46 +0100

    Add project files.

---
+++ Repository/LeaveTypeRepository.cs
@@ -0,0 +1,53 @@
+﻿using Leave_management.Contracts;
+using Leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_management.Repository
+{
+    public class LeaveTypeRepository : ILeaveTypeRepository
+
+    {
+        private readonly ApplicationDbContext _db;
+        public LeaveTypeRepository(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
+        public bool create(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveType> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeaveType FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<LeaveType> GetEmployeesByLeaveType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(LeaveType entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
